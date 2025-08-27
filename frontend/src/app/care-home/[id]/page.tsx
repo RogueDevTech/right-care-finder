400c47f20c8bd3ea5bbff3b5b8d19eef56f28caa
mohkabir <kbshowkb@gmail.com>
2025-08-27 21:42:12 +0100

    fix fix listings

--- frontend/src/app/care-home/[id]/page.tsx
+++ frontend/src/app/care-home/[id]/page.tsx
@@ -46,7 +46,7 @@ export default function CareHomeDetailsPage() {
     };
 
     fetchCareHome();
-  }, [careHomeId, getCareHomeById, router]);
+  }, [careHomeId, router]);
 
   if (isLoading) {
     return (
